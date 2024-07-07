4c1ec1d8f2f12f7c3a8901e2e34722bfa9354725
Sreejith <sreeju1762@gmail.com>
2024-07-07 20:21:54 +0530

    Update MainActivity.java

--- app/src/main/java/com/android/translator_hin_eng/MainActivity.java
+++ app/src/main/java/com/android/translator_hin_eng/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void afterTextChanged(Editable s) {
 
-                // TODO Auto-generated method stub
+                
             }
         });
 
